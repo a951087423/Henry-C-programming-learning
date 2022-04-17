1217feeca7ce23884ac3ca25bf5cef3bea52b853
a951087423 <v0973570232@gmail.com>
2022-04-17 12:09:13 +0800

    update somethings

--- C_program.c
+++ C_program.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello world\n");
+    printf("Hello World!");
+    return 0;
 }
+
+
+/*
+    常見的跳脫序列
+    \n 換行
+    \' 單引號
+    \" 雙引號
+    \\ 反斜線
+    \t 跳位
+*/
