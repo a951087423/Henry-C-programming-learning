a6a096a51fe8803b6ac8afc8755cec6cf55009ab
a951087423 <v0973570232@gmail.com>
2022-04-17 23:33:56 +0800

    update training.c

--- training.c
+++ training.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("H\tH\tA\n");
-    printf("H\tH      A A\n");
-    printf("HHHHHHHHH     A   A\n");
-    printf("H\tH    AAAAAAA\n");
-    printf("H\tH    A     A\n");
+    printf("H       H     A\n");
+    printf("H       H    A A\n");
+    printf("HHHHHHHHH   A   A\n");
+    printf("H       H  AAAAAAA\n");
+    printf("H       H  A     A\n");
+
+    printf("H       H    A\n"
+            "H       H   A A\n"
+            "HHHHHHHHH  A   A\n"
+            "H       H AAAAAAA\n"
+            "H       H A     A");
     return 0;
 }
