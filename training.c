efb9a4721cde4047f82d3357abe1374a811823b4
a951087423 <v0973570232@gmail.com>
2022-04-17 20:52:23 +0800

    add somethings

---
+++ training.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("H\tH\tA\n");
+    printf("H\tH      A A\n");
+    printf("HHHHHHHHH     A   A\n");
+    printf("H\tH    AAAAAAA\n");
+    printf("H\tH    A     A\n");
+    return 0;
+}
